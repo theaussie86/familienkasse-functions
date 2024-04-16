fa46c3dc3adf822f0b071f7b6808e9e3a21eba80
Christoph Weissteiner <30799919+theaussie86@users.noreply.github.com>
2024-04-16 09:43:09 +0200

    updated cloud function

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const { MongoClient } = require("mongodb");
 
-const client = new MongoClient(process.env.MONGO_URL ?? "");
+const client = new MongoClient(process.env.MONGO_URL);
 
 exports.insertData = async (req, res) => {
   if (req.method !== "POST") {
     res.status(405).send("Method Not Allowed");
+    return;
+  }
+
+  if (!req.body) {
+    res.status(400).send("Request body is missing");
+    return;
+  }
+
+  if (
+    !req.body.account ||
+    !["spenden", "sparen", "investieren"].includes(
+      req.body.account.trim().toLowerCase()
+    )
+  ) {
+    res.status(400).send("account field is missing  or invalid");
+    return;
+  }
+
+  if (!req.body.amount) {
+    res.status(400).send("amount field is missing");
+    return;
+  }
+
+  if (!req.body.description) {
+    res.status(400).send("description field is missing");
     return;
   }
 
@@ -15,6 +40,7 @@ exports.insertData = async (req, res) => {
     const result = await collection.insertOne({
       ...req.body,
       created: new Date(req.body.created),
+      isPaid: req.body.isPaid === "true" ? true : false,
     });
     res.status(200).send(`Data inserted with ID: ${result.insertedId}`);
   } catch (e) {
